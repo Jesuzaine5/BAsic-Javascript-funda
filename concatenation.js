74dc9cbe9fe5518b78f27558484b9a145b6dd21b
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-07 16:37:15 +0800

    Add files via upload
    concatenation tutorials

---
+++ concatenation.js
@@ -0,0 +1,12 @@
+
+//Contatenating strings with other data types will automatically convert them iunto their string countereparts
+
+//String
+const name1 = "Lisa";
+//Number
+const age = 12;
+
+//the output pf concatenationi
+console.log( "Her name is " + name1 );
+console.log("And her age is " + age + " years old");
+
