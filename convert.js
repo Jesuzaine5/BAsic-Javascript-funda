0ab84ab7e5e993da081c848fe80412f8f3c88532
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-07 21:18:32 +0800

    Add files via upload
    Let the parseInt convert your string to INTEGER

---
+++ convert.js
@@ -0,0 +1,10 @@
+//convert String to Numbers(Integer)
+
+let num1 = "12" //This is string
+const num2 = 2; //this is integer
+
+//you can convert string to interger using parseInt //should be let not const
+num1 = parseInt(num1);
+
+console.log(num1 + num2);
+
