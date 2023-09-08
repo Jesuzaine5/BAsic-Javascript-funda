66304cd6a37ab504202e22d8c6d1e7d9680c0fd3
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-08 19:50:36 +0800

    Add files via upload

---
+++ prefix.js
@@ -0,0 +1,7 @@
+
+//This is a simple of prefix
+
+let num = 0;
+
+console.log(++num);//it should read the first which is prefix meaning unang babasahin yun ++ bago ang num //output is 1
+console.log(num); //output of this is 1
