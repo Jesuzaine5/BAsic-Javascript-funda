3411977cffe6e9260068a2e149e7fbaf0629abcb
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-11 19:13:11 +0800

    Add files via upload

---
+++ tutorialsChallenge.js
@@ -0,0 +1,16 @@
+
+let num2 = "12";
+const num1 = 2;
+const name1 = "Heya";
+
+num2 = parseInt(num2)
+
+console.log("addition: " + ((num1 + num2 )/num2 + num2)-num1);
+
+
+
+
+
+
+
+
---
+++ tutorialsChangllege2.js
@@ -0,0 +1,12 @@
+let math =  86;
+let english = 87;
+let filipino = 88;
+let pe = 89;
+let history = 89;
+let ave = 5;
+
+ math + english + filipino + pe + history/5 = ave;
+ 
+
+console.log(ave);
+
