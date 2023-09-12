1a7950326531f98edf8fa9a3a168e2f7d44d25bd
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-12 19:37:04 +0800

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+//Array Lengh and reading array
+
+let names = ["Lisa", "jisso"];
+
+console.log(names.length); // it will cpount the number of elements in arrays and the answer is 2
+
+console.log(names[1]); // it will get the element value[1] which is jisso -- reading arrays
+
+console.log(names[0] = "Jennie"); //It will update the name from Lisa to Jennie-- updating arrays
+
+
+console.log(names[names.length] = "Rose");// it will add new names to names
+
+console.log(names); // it will print all the console log from top to bottom,
