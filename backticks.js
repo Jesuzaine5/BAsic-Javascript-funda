4e2ec3b5ccae2edd92618975b677f7e100606845
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-12 19:36:24 +0800

    Add files via upload

---
+++ backticks.js
@@ -0,0 +1,9 @@
+//it can be used in console.log to make it easier
+let name1 = "Lisa";
+console.log(`Hi, ${name1}`); //you can use ${} to add expression such as variables or mathematical expression instead of + sign
+
+let num1 = 5;
+let num2 = 6;
+let num3 = 7;
+let sum = num1+num2+num3;
+console.log(`${num1} + ${num2} = ${num1+num3}`); // you can use mathematical expression
