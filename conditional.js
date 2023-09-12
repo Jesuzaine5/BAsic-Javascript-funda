5809db9ef5533bbf9dec87646353b8a04a1e5c79
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-12 19:37:26 +0800

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,14 @@
+// if else statement
+
+let age = 18;
+
+if(age >= 18){
+    console.log("Legal age"); //this will print if the age is >=18
+}
+
+else {
+
+    console.log("Minor age");
+}
+
+console.log("Thank you");
