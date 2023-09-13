c49ac04dc8948c6b51822c6948e5eb17d0bd17a7
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-13 11:10:44 +0800

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,9 @@
+let age = 18;
+let experience = 3;
+let hasDegree = true;
+
+console.log(age >= 18 && experience >1); //this is an example of AND operator 
+
+console.log(age >= 18 || experience >1); // //this is an example of OR operator 
+
+console.log(!hasDegree); //this is an example of NOT operator// this will invert the result instead of true, it will be false
