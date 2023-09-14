27dc5e3a4bc9d4d347e559a56e4d6953a968fc62
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-14 12:03:32 +0800

    Add files via upload
    loop

--- forloop.js
+++ forloop.js
@@ -1,6 +1,6 @@
 let people = ["Lisa", "Jennie", "Jisoo","Rose"];  //this is a sample of array
 
-for(let i = 0; i < people.length; i++){ // for(declaration; condition; operato)
+for(let i = 0; i < people.length; i++){ // for(declaration; condition; operato) // its still the same(LET i = people.length-1; i >=0; i++)
     console.log(people[i]);  
-    if(i == 1) break;
+    
 }
