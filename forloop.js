75357a053b3fa4d023f75d5c5f02d11a180b6d6e
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-14 10:54:04 +0800

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,6 @@
+let people = ["Lisa", "Jennie", "Jisoo","Rose"];  //this is a sample of array
+
+for(let i = 0; i < people.length; i++){ // for(declaration; condition; operato)
+    console.log(people[i]);  
+    if(i == 1) break;
+}
