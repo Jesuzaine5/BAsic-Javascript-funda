db062b13865ee6a840e911d83f0f13d540eec41a
CoderGirl <143799278+Jesuzaine5@users.noreply.github.com>
2023-09-22 11:28:10 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+//number
+function add(num1, num2, num3){
+        console.log(num1 +num2 +num3);
+
+}
+//string
+function output(str){
+    console.log(str);
+}
+
+//boolean
+function boolFunc(bool1){
+    console.log(bool1);
+}
+boolFunc(true);
